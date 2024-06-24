b9963209b36c111a76abd0bfd5a456c37679149d
nastyaFR <nastenjkaumnica@mail.ru>
2024-06-24 17:05:25 +0300

    Добавляет дз 2

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+// Функция для проверки длины строки
+const checkLengthString = (string, maxLength) => {
+  const correctLengthString = string.length <= maxLength;
+  return correctLengthString;
+};
+
+checkLengthString();
+
+// Функция для проверки, является ли строка палиндромом
+function checkPalindrome (string) {
+  string = string.replaceAll(' ', '').toLowerCase();
+  const normalizeString = string;
+  let backString = '';
+  for (let i = normalizeString.length - 1; i >= 0; i--) {
+    backString += normalizeString.at(i);
+  }
+  const palindrome = (backString === normalizeString);
+  return palindrome;
+}
+
+checkPalindrome ();
