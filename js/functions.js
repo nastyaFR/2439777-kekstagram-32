98052b62edaa912c3649710c7c041e3657cd8022
nastyaFR <nastenjkaumnica@mail.ru>
2024-06-25 21:24:09 +0300

    Исправляет замечания

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,14 @@
 // Функция для проверки длины строки
 const checkLengthString = (string, maxLength) => string.length <= maxLength;
 
+checkLengthString();
+
 // Функция для проверки, является ли строка палиндромом
-const checkPalindrome = (string) => {
+const isPalindrome = (string) => {
   const cleaned = string.replaceAll(' ', '').toLowerCase();
   const normalizeString = cleaned.split('').reverse().join('');
 
   return cleaned === normalizeString;
 };
+
+isPalindrome();
