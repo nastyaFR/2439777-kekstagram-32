cd81fc796a05c700cbd7ff5836b55010effcd9fb
nastyaFR <nastenjkaumnica@mail.ru>
2024-08-07 00:25:14 +0300

    Добавляет дз 11.14

--- js/util.js
+++ js/util.js
@@ -1,20 +1,14 @@
-const getRandomInteger = (a,b) => {
-  const lower = Math.ceil(Math.min(a,b));
-  const upper = Math.floor(Math.max(a,b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+const ALERT_SHOW_TIME = 5000;
+
+const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+
+const showAlert = () => {
+  const dataErrorElement = dataErrorTemplate.cloneNode(true);
+  document.body.append(dataErrorElement);
+
+  setTimeout(() => {
+    dataErrorElement.remove();
+  }, ALERT_SHOW_TIME);
 };
 
-const getRandomArrayElement = (items) =>
-  items[getRandomInteger(0, items.length - 1)];
-
-const createIdGenerator = () => {
-  let numberId = 0;
-
-  return () => {
-    numberId += 1;
-    return numberId;
-  };
-};
-
-export {getRandomInteger,getRandomArrayElement, createIdGenerator};
+export {showAlert};
